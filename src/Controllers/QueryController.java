f1ba20eb20ef7da420779262f1f75a3aa7455d54
petethemeat <peterpaiste@gmail.com>
2017-03-27 13:33:11 -0500

    Updated Querycontroller to read field values from comma separated configuration key, field values can be retreived from Hub.getFields();

--- src/Controllers/QueryController.java
+++ src/Controllers/QueryController.java
@@ -11,16 +11,18 @@ public class QueryController
 		dbSource source = Hub.getSource();
 		
 		dbConnection connection = new dbConnection(source);
+	
+		connection.startConnection();
+		String[] fields = Hub.getFields();
+	
 		
 		//This depends on what we are trying to display.
-		String[] results = new String[4];	
-		
+		String[] results = new String[fields.length];	
+		for(int i = 0; i < fields.length; i++)
+		{
+			results[i] = connection.getField(id, fields[i]);
+		}
 
-		connection.startConnection();
-		results[0] = connection.getField(id, "id");
-		results[1] = connection.getField(id, "blade_size");
-		results[2] = connection.getField(id, "start_date");
-		results[3] = connection.getField(id, "hours_used");
 		connection.closeConnection();
 			
 	
